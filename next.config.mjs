c8e475a5d8bab5749cbc2670ef6ef0fdc6cb4eee
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2025-08-17 02:57:21 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
-    assetPrefix: isProd ? '/HTML5-Canvas-Simulations/' : '',
-    basePath: isProd ? '/HTML5-Canvas-Simulations' : '',
 };
 
 export default nextConfig;
