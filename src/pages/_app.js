4af4c66d7d800a7a87ea82d4d7f903c860878594
Rajiv Nayan Choubey <rajivnayanc@gmail.com>
2025-08-18 01:25:14 +0530

    add titles to all the subpages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import Head from 'next/head'
 import '../app/globals.css'
- 
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Awesome Simulations</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
